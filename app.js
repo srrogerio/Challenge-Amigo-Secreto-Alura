fa358f8c81fdc7c002040c7ec1ce396f0dd46dd6
srrogerio <srrogerio@gmail.com>
2025-08-02 12:06:06 -0300

    terminei a implementacao das funcoes

--- app.js
+++ app.js
@@ -1,14 +1,74 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. 
+// Aqui você deverá desenvolver a lógica para resolver o problema.
+// ------------------------------------------------------------------------------------------------
 
-/*
-############  Variáveis globais   ################
-*/
+//############  Variáveis   ################
+let listaAmigos = [] // array para armazenar os nomes dos amigos
+let inputAmigo = document.getElementById('amigo'); // variável para receber o nome do amigo no input
+let elementoListaLi = document.getElementById('listaAmigos'); // variável para receber o array
+let listaHtmlDosAmigos = '' // variável para criar o item li com os amigos
+let nomeHtmlDoAmigo = '' // variável para criar o item li com o amigo sorteado
+let indiceAleatorio = ''; // variável para o índice aleatório
+let amigoSorteado = ''; // variável do amigo sorteado
+
+
+//##########  Funções do sistema ###########
+// Função para agregar amigos
+function adicionarAmigos(){
+    if(inputAmigo.value ==''){
+        alert('Por favor, insira um nome e clique no botão "Adicionar" !!!')
+    } else {
+        listaAmigos.push(inputAmigo.value);
+        atualizaListaAmigos();
+        inputAmigo.value = '';
+        inputAmigo.focus();
+    }
+}
+
+// Função para atualizar a lista de amigos no elemento li do ul
+function atualizaListaAmigos(){ 
+    for (let i = (listaAmigos.length -1); i < listaAmigos.length; i++) { // pega o próximo nome para acrescentar no <li>
+        listaHtmlDosAmigos += "<li>" + listaAmigos[i] + "</li>";
+        document.getElementById('listaAmigos').innerHTML = listaHtmlDosAmigos;
+    }
+}
+
+// Função para sortear um amigo
+function sortearAmigo(){
+    if (listaAmigos.length < 2){
+        alert('É necessário um mínimo de dois nomes para realizar o sorteio!!!')
+    } else {
+        indiceAleatorio = Math.floor(Math.random() * listaAmigos.length);
+        amigoSorteado = listaAmigos[indiceAleatorio];
+        nomeHtmlDoAmigo += "<li>" + 'O amigo sorteado é: ' + amigoSorteado + "</li>";
+        document.getElementById('resultado').innerHTML = nomeHtmlDoAmigo;
+        inputAmigo.focus();
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* 
+
+//############  Variáveis   ################
+
 let listaDeAmigos = [];
 
 
-/*
-##################      Funções do sistema    #############
-*/
+
+//##################      Funções do sistema    #############
+
 
 // Função para limpar o input
 function limparInput(){
@@ -35,6 +95,9 @@ function exibirAmigos(){
     // Obtém a referência para a lista ul existente.
     let listaUlDeAmigos = document.getElementById('listaAmigos'); 
 
+    // Limpa a lista antes de adicionar novos elementos.
+    listaDeAmigos.innerHTML = '';
+
     // Cria um novo item para a lista ul.
     let novoItem = document.createElement("li");  
     
@@ -46,6 +109,7 @@ function exibirAmigos(){
     listaUlDeAmigos.appendChild(novoItem);
 }
 
+*/
 
 
  
